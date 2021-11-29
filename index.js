0125f344dee0ac12d111a28ac062467fe98c8c40
jportillo8 <buttokk@gmail.com>
2021-11-29 17:36:33 -0500

    Fin seccione 3

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const path = require('path');
+require('dotenv').config();
+
+// App de Express
+const app = express();
+
+// Sokect node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+
+require('./sockets/sokets');
+
+
+
+// Path publico
+const publicPath = path.resolve( __dirname, 'public');
+app.use(express.static( publicPath ));
+
+server.listen( process.env.PORT, (err) => {
+
+    if( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en el puerto', process.env.PORT);
+
+})
